90017d48deb2b2df08ac900e230883d44d7701b7
www0054 <hxhky018@gmail.com>
2023-11-10 10:31:32 +0800

    类型检查语义分析和中间代码生成

--- src/cn/edu/hitsz/compiler/parser/IRGenerator.java
+++ src/cn/edu/hitsz/compiler/parser/IRGenerator.java
@@ -1,6 +1,9 @@
 package cn.edu.hitsz.compiler.parser;
 
 import cn.edu.hitsz.compiler.NotImplementedException;
+import cn.edu.hitsz.compiler.ir.IRImmediate;
+import cn.edu.hitsz.compiler.ir.IRValue;
+import cn.edu.hitsz.compiler.ir.IRVariable;
 import cn.edu.hitsz.compiler.ir.Instruction;
 import cn.edu.hitsz.compiler.lexer.Token;
 import cn.edu.hitsz.compiler.parser.table.Production;
@@ -8,7 +11,9 @@ import cn.edu.hitsz.compiler.parser.table.Status;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 import cn.edu.hitsz.compiler.utils.FileUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 // TODO: 实验三: 实现 IR 生成
 
@@ -17,34 +22,71 @@ import java.util.List;
  */
 public class IRGenerator implements ActionObserver {
 
+    SymbolTable table;
+    List<IRValue> Stack = new ArrayList<>();
+    List<Instruction> IRList = new ArrayList<>();
+
+    IRVariable temp;
+
     @Override
     public void whenShift(Status currentStatus, Token currentToken) {
-        // TODO
-        throw new NotImplementedException();
+        if(currentToken.getKind().getIdentifier().equals("id")){
+            Stack.add(IRVariable.named(currentToken.getText()));
+        }
+        else if(currentToken.getKind().getIdentifier().equals("IntConst")){
+            Stack.add(IRImmediate.of(Integer.parseInt(currentToken.getText())));
+        }
     }
 
     @Override
     public void whenReduce(Status currentStatus, Production production) {
-        // TODO
-        throw new NotImplementedException();
+        switch (production.index()){
+            case 6:
+                IRList.add(Instruction.createMov((IRVariable) Stack.get(Stack.size()-2),Stack.get(Stack.size()-1)));
+                Stack.remove(Stack.size()-1);
+                Stack.remove(Stack.size()-1);
+                break;
+            case 7:
+                IRList.add(Instruction.createRet(Stack.get(Stack.size()-1)));
+                Stack.remove(Stack.size()-1);
+                break;
+            case 8:
+                temp = IRVariable.temp();
+                IRList.add(Instruction.createAdd(temp, Stack.get(Stack.size()-2),Stack.get(Stack.size()-1)));
+                Stack.remove(Stack.size()-1);
+                Stack.remove(Stack.size()-1);
+                Stack.add(temp);
+                break;
+            case 9:
+                temp = IRVariable.temp();
+                IRList.add(Instruction.createSub(temp, Stack.get(Stack.size()-2),Stack.get(Stack.size()-1)));
+                Stack.remove(Stack.size()-1);
+                Stack.remove(Stack.size()-1);
+                Stack.add(temp);
+                break;
+            case 11:
+                temp = IRVariable.temp();
+                IRList.add(Instruction.createMul(temp, Stack.get(Stack.size()-2),Stack.get(Stack.size()-1)));
+                Stack.remove(Stack.size()-1);
+                Stack.remove(Stack.size()-1);
+                Stack.add(temp);
+                break;
+        }
     }
 
 
     @Override
     public void whenAccept(Status currentStatus) {
-        // TODO
-        throw new NotImplementedException();
+        Stack.clear();
     }
 
     @Override
     public void setSymbolTable(SymbolTable table) {
-        // TODO
-        throw new NotImplementedException();
+        this.table = table;
     }
 
     public List<Instruction> getIR() {
-        // TODO
-        throw new NotImplementedException();
+        return IRList;
     }
 
     public void dumpIR(String path) {
